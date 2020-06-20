4d548b77b76a0c76dc2bb4f9182474bc6f8e4ec2
Serhii Diachenko <Sergey.Dyachenko@gmail.com>
2020-06-20 21:05:33 +0300

    Add device-vendor.mk
    Signed-off-by: Serhii Diachenko <Sergey.Dyachenko@gmail.com>

---
+++ device-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_PACKAGES += \
+	LedControlService \
+	LedControlApplication \
+
+DEVICE_MANIFEST_FILE += \
+	vendor/gl-serggio-26/hardware/interfaces/manifest.xml
+
+DEVICE_MATRIX_FILE += \
+	vendor/gl-serggio-26/hardware/interfaces/compatibility_matrix.xml
+	
