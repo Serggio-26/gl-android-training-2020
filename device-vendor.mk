75122a57319fe5ea38c25f0483f5f19c95157396
Serhii Diachenko <Sergey.Dyachenko@gmail.com>
2020-06-28 08:10:12 +0300

    Add HAL to device-vendor.mk
    Signed-off-by: Serhii Diachenko <Sergey.Dyachenko@gmail.com>

--- device-vendor.mk
+++ device-vendor.mk
@@ -1,10 +1,17 @@
+# Hal
+PRODUCT_PACKAGES += \
+	vendor.gls.hardware.LedControl@1.0-service \
+
+DEVICE_MANIFEST_FILE += \
+	vendor/gls/hardware/interfaces/manifest.xml
+
+DEVICE_MATRIX_FILE += \
+	vendor/gls/hardware/interfaces/compatibility_matrix.xml
+
+TARGET_FS_CONFIG_GEN += \
+        vendor/gls/hardware/interfaces/config.fs
+
+
 PRODUCT_PACKAGES += \
 	LedControlService \
 	LedControlApplication \
-
-DEVICE_MANIFEST_FILE += \
-	vendor/gl-serggio-26/hardware/interfaces/manifest.xml
-
-DEVICE_MATRIX_FILE += \
-	vendor/gl-serggio-26/hardware/interfaces/compatibility_matrix.xml
-	
